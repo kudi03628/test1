e7d92de1b191e56374d3eee6308f701c19a2767f
kudi03628 <153245910+kudi03628@users.noreply.github.com>
2024-03-10 18:29:43 +0900

    Initial commit

---
+++ src/test1/Test1.java
@@ -0,0 +1,89 @@
+package test1;
+
+
+import java.util.Scanner;
+
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	
+	Scanner sc = new Scanner(System.in);
+		
+	String input_main_num = "";
+		
+		do {
+			
+			System.out.println("[도서 대여관리] 프로그램 입니다.");
+			
+			System.out.println("로그인된 회원만 이용 할 수 있습니다.\n");
+			
+			System.out.println("1.회원가입" + "    " 
+							  +"2.도서회원 로그인" + "    "  
+							  +"3.회원탈퇴" + "    " 
+							  +"4.프로그램 종료\n");
+			
+			System.out.print("원하시는 메뉴 번호를 입력해주세요 >>");
+			
+			
+			
+			input_main_num = sc.nextLine();
+			
+			switch (input_main_num) {
+			
+			case "1":
+				
+				
+				
+				
+				break;
+				
+			case "2":
+				
+				
+				
+				break;	
+			
+			case "3":
+		
+			
+				
+				break;	
+				
+			case "4":
+				
+				
+				
+				break;
+			
+			case "5":	
+				
+				System.out.println("\n[도서 대여관리] 프로그램을 종료합니다!");
+				
+				
+				
+				break;
+				
+		
+			default:
+				
+				System.out.println("\n메뉴에 존재하는 번호만 입력해주세요!\n");
+				
+				break;
+				
+			} // end of main_switch
+			
+			
+		
+		} // end of main_do_while 
+		while (! ("4".equals(input_main_num) ) );
+		
+		sc.close();
+
+	}
+
+}
